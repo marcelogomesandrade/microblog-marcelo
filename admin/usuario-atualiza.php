79db01a0271c71fdf9c8bc68f9e933dc3ef0dc13
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-11-21 16:51:26 -0300

    ====

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -1,9 +1,8 @@
-
 <?php //usuario atualiza. php 
 require_once "../inc/funcoes-usuarios.php";
 require_once "../inc/cabecalho-admin.php";
 
-$id = $_GET ['id'];
+$id = $_GET['id'];
 
 $usuario = lerUmUsuario($conexao, $id);
 ?>
@@ -12,21 +11,21 @@ $usuario = lerUmUsuario($conexao, $id);
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Atualizar dados do usuário
+			Atualizar dados do usuário
 		</h2>
-				
+
 		<form class="mx-auto w-75" action="" method="post" id="form-atualizar" name="form-atualizar">
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input value ="<?=$usuario['nome']?>"class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?= $usuario['nome'] ?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input value ="<?=$usuario['email']?>"class="form-control" type="email" id="email" name="email" required>
+				<input value="<?= $usuario['email'] ?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
@@ -40,26 +39,21 @@ $usuario = lerUmUsuario($conexao, $id);
 					<option value=""></option>
 
 
-					<option 
-					<?php if($usuario ["tipo"]=== "editor") echo "selected";?>
-					 value="editor">Editor</option>
+					<option <?php if ($usuario["tipo"] === "editor") echo "selected"; ?> value="editor">Editor</option>
 
 
-					<option 
-					<?php if($usuario ["tipo"]=== "admin") echo "selected";?>
-					value="admin">Administrador</option>
+					<option <?php if ($usuario["tipo"] === "admin") echo "selected"; ?> value="admin">Administrador</option>
 
 				</select>
 			</div>
-			
+
 			<button class="btn btn-primary" name="atualizar"><i class="bi bi-arrow-clockwise"></i> Atualizar</button>
 		</form>
-		
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../inc/rodape-admin.php";
 ?>
-
