8989bad5378c624294aae02c3b0ed5bd62a65c54
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-11-22 14:26:57 -0300

    op

---
+++ admin/usuario-exclui.php
@@ -0,0 +1,6 @@
+<?php
+
+require "../inc/funcoes-usuarios.php";
+$id = $_GET ['id'];
+excluirUsuario($conexao, $id);
+header ("location:usuarios.php");
--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -60,3 +60,10 @@ function atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo)
 
     mysqli_query($conexao, $sql) or die (mysqli_error($conexao));
 }
+
+function excluirUsuario ($conexao,$id) {
+
+    $sql = "DELETE FROM usuarios WHERE id = $id";
+
+    mysqli_query($conexao, $sql) or die (mysqli_error($conexao));
+}
