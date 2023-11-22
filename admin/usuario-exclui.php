37c42f54d66c24f58b67e82de367ef83e6b32f06
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-11-22 15:16:18 -0300

    uop

--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -1,6 +1,10 @@
 <?php
 
+require "../inc/funcoes-sessao.php";
 require "../inc/funcoes-usuarios.php";
+
+verificaAcesso();
+
 $id = $_GET ['id'];
 excluirUsuario($conexao, $id);
 header ("location:usuarios.php");
--- inc/funcoes-sessao.php
+++ inc/funcoes-sessao.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* Sessões no PHP 
 Recurso usado para o controle de acesso á determinadas páginas e ou recursos do site. 
 
@@ -9,19 +9,37 @@ Exemplos: login/senha, digital, facial, etc... */
 
 /* Verificar se já NÃO EXISTE uma sessão em funcionamento */
 
-if (!isset($_SESSION)){
+if (!isset($_SESSION)) {
     // Então inicie a sessão 
     session_start();
+}
+
+function verificaAcesso()
+{
+    /*Se NÃO EXISTIR uma variável de sessão chamada "id" baseaada no id de um usuário logado, então significa que ele/ela NÃO ESTÁ LIGADA(0) no sistema. */
+
+    if (!isset($_SESSION['id'])) {
+        /* Portanto , destrua os dados da sessão, redicione para a página login.php e pare o stript.*/
+        session_destroy();
+        header("location:../login.php?acesso_negado");
+        exit; // ou die ()
+    }
+}
+
+function login($id, $nome, $tipo)
+{
+    /*Criação de variáveis de sessão - Recursos que ficam disponíveis para o uso durante toda a duração da sessão, ou seja, enquanto o navegador não for fechado ou o usuário não clicar em sair. */
+
+    $_SESSION["id"]= $id;
+    $_SESSION["nome"]= $nome;
+    $_SESSION["tipo"]= $tipo;
 
 }
 
-function verificaAcesso (){
-    /*Se NÃO EXISTIR uma variável de sessão chamada "id" baseaada no id de um usuário logado, então significa que ele/ela NÃO ESTÁ LIGADA(0) no sistema. */
+function logout () {
+
+    session_destroy();
+    header("location:../login.php?saiu");
+    exit; //ou die()
     
-    if(!isset($_SESSION['id'])){
-        /* Portanto , destrua os dados da sessão, redicione para a página login.php e pare o stript.*/
-        session_destroy();
-        header ("location:../login.php?acesso_negado");
-        exit;// ou die ()
-    }
 }
