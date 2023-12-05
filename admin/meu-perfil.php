3e0ced9c42856c0a5257d6328570a9fc47c39e45
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-12-05 17:26:38 -0300

    9999

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,6 +1,50 @@
-<?php 
+<?php //usuario meu perfil. php 
+require_once "../inc/funcoes-usuarios.php";
 require_once "../inc/cabecalho-admin.php";
+
+
+
+$id = $_SESSION['id'];
+
+// Chamamos a função e guardando o retorno dela
+$usuario = lerUmUsuario($conexao, $id);
+// Verificando se o formulário foi acionado
+if (isset($_POST['atualizar'])) {
+
+	//Capturando os dados
+
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$tipo = $_SESSION['tipo'];// mantenha o tipo 
+
+	// Lógica para a senha 
+	// Se o campo senha estiver vazio OU se a senha digitada for igual á senha que já existe no banco de dados, então, significa que o usuário NÃO ALTEROU A SENHA. Portanto, devemos MANTER a senha existente.
+
+	if (empty($_POST['senha']) || password_verify($_POST['senha'],$usuario['senha']) ) {
+		$senha = $usuario['senha']; //mantendo a mesma
+
+
+	} else {
+	// Caso contrário, pegaremos a senha nova digitada e a codificamos antes de mandar para o banco.
+	$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+}
+ atualizarUsuario($conexao, $id,$nome,$email,$senha,$tipo
+
+);
+
+// Atualize na sessão atual o nome da pessoa (caso mude)
+
+$_SESSION ['nome'] = $nome;
+
+//Redirecionamos para a página de usuários 
+
+header("location:index.php"); 
+
+}
+
 ?>
+
+
 
 
 <div class="row">
@@ -14,12 +58,12 @@ require_once "../inc/cabecalho-admin.php";
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value = "<?=$usuario['nome']?>"class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value = "<?=$usuario['email']?>"class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
